5fd0ec4012df7c615cc2f453e41642af242f028a
Nocturnal <contact@nocturnal.solutions>
2017-07-15 21:46:14 -0600

    Add .git to Kitura URL in Package.swift to (hopefully) stop occasional SPM "node is produced by multiple commands" errors.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "KituraLangNeg",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
     ]
 )
